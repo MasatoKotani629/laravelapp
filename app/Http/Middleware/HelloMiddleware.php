d8b313a34bc22517cc737881f6c9156cf7ce9071
unknown <ma-kotani@DESKTOP-VQV6T2J.taika.local>
2020-01-23 14:13:36 +0900

    Preprocessing

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -15,6 +15,12 @@ class HelloMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $data = [
+            ['name' => 'taro', 'mail' => 'taro@yamada'],
+            ['name' => 'taro1', 'mail' => 'taro1@yamada'],
+            ['name' => 'taro2', 'mail' => 'taro2@yamada'],
+        ];
+        $request->merge(['data'=>$data]);
         return $next($request);
     }
 }
